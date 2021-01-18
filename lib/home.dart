c40cbe163a2067316712fb45be2edecba4769c27
Satit Rianpit <siteslave@msn.com>
2021-01-18 14:21:19 +0700

    update

--- lib/home.dart
+++ lib/home.dart
@@ -9,6 +9,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFEDF0F2),// Color(0xFFxxxxxx)
         appBar: AppBar(
           title: Text('SPS Application'),
         ),
@@ -30,19 +31,30 @@ class _HomeState extends State<Home> {
             ),
 
             Container(
-              height: 100,
-              color: Colors.yellow,
+              // height: 100,
+              margin: EdgeInsets.only(left: 10, right: 10),
+              padding: EdgeInsets.all(10),
+              color: Colors.white,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                Text('Google Express -- 15 mins ago'),
-                Text('Package Shipped!', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
-                Row(
-                  children: [
-                    Icon(Icons.notification_important),
-                    Text('Cucumber Mask Facial has shipped'),
-                  ],
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: Text('Google Express -- 15 mins ago'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: Text('Package Shipped!', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: Row(
+                    children: [
+                      Icon(Icons.notification_important),
+                      Text('Cucumber Mask Facial has shipped'),
+                    ],
+                  ),
                 )
               ],),
             ),
