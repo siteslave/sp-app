bfc924f9cbad0312f42416d840680b54827f05fd
Satit Rianpit <siteslave@msn.com>
2021-01-18 13:27:59 +0700

    add button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sps_app/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,11 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SPS Application',
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('SPS Application'),
-          ),
-          body: Text('xxxxx')),
+      home: Home()
     );
   }
 }
