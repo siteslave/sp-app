811731df423d58a0238e7e16ee5794341525ee6b
Satit Rianpit <siteslave@msn.com>
2021-01-19 11:09:36 +0700

    add info

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -13,6 +13,15 @@ class _HistoryState extends State<History> {
     ), body: ListView(
       children: [
         Image(image: AssetImage('assets/images/hospital.jpg'), height: 100,),
+
+      Column(children: [
+        Text('โรงพยาบาลสรรพสิทธิประสงค์',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
+        Text('Sunpasitthiprasong Hospital'),
+        ListTile(leading: Text('ประเภท', style: TextStyle(fontWeight: FontWeight.bold)), title: Text('รัฐ (โรงพยาบาลศูนย์)')),
+        ListTile(leading: Text('ที่ตั้ง', style: TextStyle(fontWeight: FontWeight.bold)), title: Text('เลขที่ 122 ถนนสรรพสิทธิ์ ตำบลในเมือง อำเภอเมืองอุบลราชธานี จังหวัดอุบลราชธานี')),
+
+      ]),
+
       ],
     ),);
   }
