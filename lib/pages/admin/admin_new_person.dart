4c9e17dfb6c9547eedb6c552c79a915bf9636411
Satit Rianpit <siteslave@msn.com>
2021-01-19 15:58:21 +0700

    add new person page

---
+++ lib/pages/admin/admin_new_person.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AdminNewPerson extends StatefulWidget {
+  @override
+  _AdminNewPersonState createState() => _AdminNewPersonState();
+}
+
+class _AdminNewPersonState extends State<AdminNewPerson> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('เพิ่มข้อมูลบุคลากร')
+      ),
+      body: Text('ปปปปป')
+    );
+  }
+}
--- lib/pages/manager.dart
+++ lib/pages/manager.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sps_app/pages/admin/admin_new_person.dart';
 
 class Manager extends StatefulWidget {
   @override
@@ -9,79 +10,96 @@ class _ManagerState extends State<Manager> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('ทำเนียบผู้บริการ'),),
-      body: ListView(
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10, ),
-            height: 150,
-            width: 150,
-            decoration: BoxDecoration(
-              border: Border.all(width: 3, color: Colors.grey[300]),
-              shape: BoxShape.circle,
-              color: Colors.white,
-              image: DecorationImage(
-                image: AssetImage('assets/images/mamager.png'),
-              )
-            ),
-          ),
-          Center(child: Text('นายมนต์ชัย วิวัฒนาสิทธิพงศ์', style: TextStyle(fontWeight: FontWeight.bold))),
-          Center(child: Text('ผู้อำนวยการโรงพยาบาล')),
-          Divider(),
-          Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: Text('รายชื่อบุคลากร', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey)),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10)
-            ),
-            child: ExpansionTile(
-              title: Text('กลุ่มงานประกันสุขภาพ'),
-              children: [
-                ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.grey[100],
-                    child: Image.network('https://randomuser.me/api/portraits/men/32.jpg'),
-                  ),
-                  title: Text('นายสถิตย์ เรียนพิศ'),subtitle: Text('นักวิชาการคอมพิวเตอร์')),
-                ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.grey[100],
-                    child: Image.network('https://randomuser.me/api/portraits/men/33.jpg'),
-                  ),
-                  title: Text('นายทดสอบ ไม่เอาจริง'),subtitle: Text('นักวิชาการสาธารณสุข')),
-              ],
+        appBar: AppBar(
+          title: Text('ทำเนียบผู้บริการ'),
+        ),
+        body: ListView(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(
+                vertical: 10,
               ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10)
+              height: 150,
+              width: 150,
+              decoration: BoxDecoration(
+                  border: Border.all(width: 3, color: Colors.grey[300]),
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/mamager.png'),
+                  )),
             ),
-            child: ExpansionTile(
-              title: Text('กลุ่มงานส่งเสริมสุขภาพ'),
-              children: [
-                ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.grey[100],
-                    child: Image.network('https://randomuser.me/api/portraits/men/34.jpg'),
-                  ),
-                  title: Text('นายสถิตย์ เรียนพิศ'),subtitle: Text('นักวิชาการคอมพิวเตอร์')),
-                ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.grey[100],
-                    child: Image.network('https://randomuser.me/api/portraits/men/35.jpg'),
-                  ),
-                  title: Text('นายทดสอบ ไม่เอาจริง'),subtitle: Text('นักวิชาการสาธารณสุข')),
-              ],
+            Center(
+                child: Text('นายมนต์ชัย วิวัฒนาสิทธิพงศ์',
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            Center(child: Text('ผู้อำนวยการโรงพยาบาล')),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text('รายชื่อบุคลากร',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.grey)),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
+              child: ExpansionTile(
+                title: Text('กลุ่มงานประกันสุขภาพ'),
+                children: [
+                  ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.grey[100],
+                        child: Image.network(
+                            'https://randomuser.me/api/portraits/men/32.jpg'),
+                      ),
+                      title: Text('นายสถิตย์ เรียนพิศ'),
+                      subtitle: Text('นักวิชาการคอมพิวเตอร์')),
+                  ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.grey[100],
+                        child: Image.network(
+                            'https://randomuser.me/api/portraits/men/33.jpg'),
+                      ),
+                      title: Text('นายทดสอบ ไม่เอาจริง'),
+                      subtitle: Text('นักวิชาการสาธารณสุข')),
+                ],
               ),
-          ),
-        ],
-      ),
-    );
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
+              child: ExpansionTile(
+                title: Text('กลุ่มงานส่งเสริมสุขภาพ'),
+                children: [
+                  ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.grey[100],
+                        child: Image.network(
+                            'https://randomuser.me/api/portraits/men/34.jpg'),
+                      ),
+                      title: Text('นายสถิตย์ เรียนพิศ'),
+                      subtitle: Text('นักวิชาการคอมพิวเตอร์')),
+                  ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.grey[100],
+                        child: Image.network(
+                            'https://randomuser.me/api/portraits/men/35.jpg'),
+                      ),
+                      title: Text('นายทดสอบ ไม่เอาจริง'),
+                      subtitle: Text('นักวิชาการสาธารณสุข')),
+                ],
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.person_add),
+          onPressed: () {
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => AdminNewPerson()));
+          },
+        ));
   }
 }
